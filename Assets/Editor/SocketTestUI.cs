3261bcacdb35ee09570f61dc9dc73064955c95d8
Alex <alex.savi.kv@gmail.com>
2024-06-30 20:17:48 +0530

    socket added

---
+++ Assets/Editor/SocketTestUI.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+using UnityEngine;
+//[CustomEditor(typeof(SocketTest))]
+public class SocketTestUI : Editor
+{
+    /*public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        SocketTest socketTest = (SocketTest)target;
+        if (GUILayout.Button("Emit Message"))
+        {
+            socketTest.EmitMessage();
+            Debug.Log("button clicked");
+        }
+    }*/
+}
